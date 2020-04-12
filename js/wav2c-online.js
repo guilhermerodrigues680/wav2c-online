463f71707b8b5a4a9856f2079738169feacb7472
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-04-12 08:14:01 -0300

    v0.3

--- js/wav2c-online.js
+++ js/wav2c-online.js
@@ -1,64 +1,66 @@
-document.getElementById('audiowav').addEventListener('change', lerArquivoWav);
-document.getElementById('button-copy-clipboard').addEventListener('click', CopyToClipboard);
-document.getElementById('button-download-h-file').addEventListener('click', generateDownloadHFile);
+//
+// WAV2C Online - Por: Guilherme Rodrigues
+//
 
-$( document ).ready(function() {
-    $('#div-acoes').hide();
-    $('#div-c-out').hide();
+
+$( document ).ready(function(){
+    $( "#div-acoes" ).hide();
+    $( "#div-c-out" ).hide();
+
+    $( "#input-audiowav" ).on( "change", lerArquivoWav);
+    $( "#button-copy-clipboard" ).on( "click", copyToClipboard);
+    $( "#button-download-h-file" ).on( "click", generateDownloadHFile);
 });
 
-function lerArquivoWav() {
-    // console.log(this.files);
 
-    document.getElementById('label-audio-upload').innerText = `${this.files[0].name} - ${this.files[0].size} bytes`;
+/** Função responsavel por ler e converte o arquivo wav em texto */
+function lerArquivoWav() {
+    // console.debug(this.files);
+    $( "#label-audio-upload" ).text(`${this.files[0].name} - ${this.files[0].size} bytes`);
 
     if (this.files.length === 0) {
-        console.log('No file selected.');
+        console.log("Nenhum arquivo selecionado.");
         return;
     }
 
     var reader = new FileReader();
     reader.onload = function(e) {
-        var audioBytes = new Uint8Array(e.target.result);
-        
+        // No WAV, os 44 bytes iniciais são de cabeçalho: https://wiki.fileformat.com/audio/wav/
+        const audioBytes = new Uint8Array(e.target.result);
         const SOUND_NAME = "sounddata";
-        const FRAMERATE = (audioBytes[27] << 24) | (audioBytes[26] << 16) | (audioBytes[25] << 8) | audioBytes[24]
-        const AUDIO_SAMPLES = audioBytes.length - 44
-        // console.log(audioBytes)
-        // console.log("INFO: sampleRate: %s , length: %d", FRAMERATE, AUDIO_SAMPLES)
+        const FRAMERATE = (audioBytes[27] << 24) | (audioBytes[26] << 16) | (audioBytes[25] << 8) | audioBytes[24];
+        const AUDIO_SAMPLES = audioBytes.length - 44;
+        // console.debug(audioBytes)
 
-        document.getElementById('p-file-info').innerHTML = `<strong>Taxa de amostragem:</strong> ${FRAMERATE} , <strong>amostras:</strong> ${AUDIO_SAMPLES}`
-        let divOut = document.getElementById('code-c-out');
-        let divStatus = document.getElementById('div-status');
+        $( "#p-file-info" ).html(`<strong>Taxa de amostragem:</strong> ${FRAMERATE} , <strong>amostras:</strong> ${AUDIO_SAMPLES}`);
+        $( "#p-status" ).text( "Iniciando a conversão" );
+
         let txt = "";
-
-        txt += "// Wav2c recriado em Javascript por Guilherme Rodrigues: https://github.com/guilhermerodrigues680/wav2c-online\n"
-        txt += "// Baseado no wav2c em C de Olle Jonsson: https://github.com/olleolleolle/wav2c\n\n"
-        txt += `// const int ${SOUND_NAME}_sampleRate = ${FRAMERATE}; \n`
-        txt += `const int ${SOUND_NAME}_length = ${AUDIO_SAMPLES}; \n\n`
-        txt += `const unsigned char ${SOUND_NAME}_data[] PROGMEM ={`
-
-        divStatus.innerHTML = "Iniciando a conversão"
+        txt += "// Wav2c recriado em Javascript por Guilherme Rodrigues: https://github.com/guilhermerodrigues680/wav2c-online\n";
+        txt += "// Baseado no wav2c em C de Olle Jonsson: https://github.com/olleolleolle/wav2c\n\n";
+        txt += `// const int ${SOUND_NAME}_sampleRate = ${FRAMERATE}; \n`;
+        txt += `const int ${SOUND_NAME}_length = ${AUDIO_SAMPLES}; \n\n`;
+        txt += `const unsigned char ${SOUND_NAME}_data[] PROGMEM ={`;
 
         audioBytes.slice(44).forEach((sample, idx, arr) => {
 
             switch (String(sample).length) {
                 case 1:
-                    txt += '  '
+                    txt += "  ";
                     break;
                 case 2:
-                    txt += ' '
+                    txt += " ";
                     break;
             }
 
-            txt += sample
+            txt += sample;
 
             if ((idx + 1) != arr.length) {
                 
                 if (idx % 20 == 0) {
-                    txt += ',\n';
+                    txt += ",\n";
                 } else {
-                    txt += ', ';
+                    txt += ", ";
                 }
 
             } else {
@@ -67,42 +69,38 @@ function lerArquivoWav() {
             
         });
 
-        divOut.innerHTML = txt;
-        divStatus.innerHTML = "Conversão finalizada"
-        $('#div-acoes').show();
-        $('#div-c-out').show();
+        $( "#code-c-out" ).html( txt );
+        $( "#p-status" ).text( "Conversão finalizada" );
+        $( "#div-acoes" ).show();
+        $( "#div-c-out" ).show();
         Prism.highlightAll();
     }
     reader.readAsArrayBuffer(this.files[0]);
 }
 
 
+/** Função responsavel por gerar um arquivo para download */
 function generateDownloadHFile() {
-    let filename = 'sounddata.h';
-    let text = document.getElementById('code-c-out').innerText;
+    const filename = "sounddata.h";
+    const text = $( "#code-c-out" ).text();
     let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-
-    element.style.display = 'none';
+    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+    element.setAttribute("download", filename);
+    element.style.display = "none";
     document.body.appendChild(element);
-
     element.click();
-
     document.body.removeChild(element);
 }
 
 
-function CopyToClipboard() {
-
-    let containerid = 'code-c-out';
-
+/** Função responsavel por copiar o codigo para area de trasnferencia */
+function copyToClipboard() {
+    let containerid = "code-c-out";
     if (document.selection) {
         var range = document.body.createTextRange();
         range.moveToElementText(document.getElementById(containerid));
         range.select().createTextRange();
         document.execCommand("copy");
-
     } else if (window.getSelection) {
         var range = document.createRange();
         range.selectNode(document.getElementById(containerid));
