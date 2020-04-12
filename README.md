8c2607d8a6b03af3c2e6a71f3818cf2f3465d92f
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-04-11 22:49:36 -0300

    v0.2

---
+++ README.md
@@ -0,0 +1,21 @@
+# wav2c-online
+Acesse o conversor em: [guilhermerodrigues680.github.io/wav2c-online](https://guilhermerodrigues680.github.io/wav2c-online)  
+
+## Descrição
+Conversor escrito em Javascript que lê um WAV Unsigned 8-bit PCM e converte em um array C/C++ para uso em microcontroladores  
+Útil para tocar sons em um microcontrolador , não sendo necessário um modulo MP3. Porém fica restrito ao tamanho de 
+memoria disponivel no microcontrolador.  
+
+
+## Exemplo de uso
+Para converter um áudio, ex: um .MP3, ultilize o software [Audacity](https://www.audacityteam.org/) para converter ele 
+em **Mono** em seguida reduza a taxa de amostragem para **8000** e exporte como **WAV Unsigned 8-bit PCM** . 
+Após gerado o arquivo carregue ele no conversor que gerará o código C/C++ .  
+
+Para o Arduino, o código de Michael Smith pode ser ultilizado para reproduzir o som.
+https://playground.arduino.cc/Code/PCMAudio/
+
+
+### Links
+- https://www.reddit.com/r/javascript/comments/nuizh/reading_raw_wave_data_in_jshtml5/
+- https://wiki.fileformat.com/audio/wav/
--- js/wav2c-online.js
+++ js/wav2c-online.js
@@ -1,22 +1,21 @@
-console.log("OK!");
-
 document.getElementById('audiowav').addEventListener('change', lerArquivoWav);
+document.getElementById('button-copy-clipboard').addEventListener('click', CopyToClipboard);
+document.getElementById('button-download-h-file').addEventListener('click', generateDownloadHFile);
 
+$( document ).ready(function() {
+    $('#div-acoes').hide();
+    $('#div-c-out').hide();
+});
 
 function lerArquivoWav() {
-    console.log(this.files); // will contain information about the file that was selected.
+    // console.log(this.files);
+
+    document.getElementById('label-audio-upload').innerText = `${this.files[0].name} - ${this.files[0].size} bytes`;
 
     if (this.files.length === 0) {
         console.log('No file selected.');
         return;
     }
-
-    // const reader = new FileReader();
-    // reader.onload = function fileReadCompleted() {
-    //     // when the reader is done, the content is in reader.result.
-    //     console.log(reader.result);
-    // };
-    // reader.readAsText(this.files[0]);
 
     var reader = new FileReader();
     reader.onload = function(e) {
@@ -25,17 +24,15 @@ function lerArquivoWav() {
         const SOUND_NAME = "sounddata";
         const FRAMERATE = (audioBytes[27] << 24) | (audioBytes[26] << 16) | (audioBytes[25] << 8) | audioBytes[24]
         const AUDIO_SAMPLES = audioBytes.length - 44
-
         // console.log(audioBytes)
-        
-        console.log("INFO: sampleRate: %s , length: %d", FRAMERATE, AUDIO_SAMPLES)
+        // console.log("INFO: sampleRate: %s , length: %d", FRAMERATE, AUDIO_SAMPLES)
 
-
-        let divOut = document.getElementById('div-c-out');
+        document.getElementById('p-file-info').innerHTML = `<strong>Taxa de amostragem:</strong> ${FRAMERATE} , <strong>amostras:</strong> ${AUDIO_SAMPLES}`
+        let divOut = document.getElementById('code-c-out');
         let divStatus = document.getElementById('div-status');
         let txt = "";
 
-        txt += "// Wav2c recriado em Javascript por Guilherme Rodrigues: https://github.com/guilhermerodrigues680/py-wav2c\n"
+        txt += "// Wav2c recriado em Javascript por Guilherme Rodrigues: https://github.com/guilhermerodrigues680/wav2c-online\n"
         txt += "// Baseado no wav2c em C de Olle Jonsson: https://github.com/olleolleolle/wav2c\n\n"
         txt += `// const int ${SOUND_NAME}_sampleRate = ${FRAMERATE}; \n`
         txt += `const int ${SOUND_NAME}_length = ${AUDIO_SAMPLES}; \n\n`
@@ -54,7 +51,6 @@ function lerArquivoWav() {
                     break;
             }
 
-            // txt += sample + ', ';
             txt += sample
 
             if ((idx + 1) != arr.length) {
@@ -71,19 +67,20 @@ function lerArquivoWav() {
             
         });
 
-        divOut.innerText = txt;
+        divOut.innerHTML = txt;
         divStatus.innerHTML = "Conversão finalizada"
-
-        generateDownloadHFile('teste.h', txt)
-
-
+        $('#div-acoes').show();
+        $('#div-c-out').show();
+        Prism.highlightAll();
     }
     reader.readAsArrayBuffer(this.files[0]);
 }
 
 
-function generateDownloadHFile(filename, text) {
-    let element = document.createElement('a')
+function generateDownloadHFile() {
+    let filename = 'sounddata.h';
+    let text = document.getElementById('code-c-out').innerText;
+    let element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
 
@@ -93,4 +90,24 @@ function generateDownloadHFile(filename, text) {
     element.click();
 
     document.body.removeChild(element);
+}
+
+
+function CopyToClipboard() {
+
+    let containerid = 'code-c-out';
+
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select().createTextRange();
+        document.execCommand("copy");
+
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        // alert("O código gerado foi copiado! Pressione Ctrl+V para colá-lo")
+    }
 }
